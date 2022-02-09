7893a50ac8ca28ac94c96c77e363ea9e1bfa68ac
Damian Kohut <damiankohut@gmail.com>
2022-02-09 14:35:30 -0500

    character

--- index.js
+++ index.js
@@ -4,7 +4,6 @@
 //TITLE
 let title = document.getElementById("title");
 
-
 //CONTAINER
 let containerPg1 = document.getElementById("container");
 
@@ -20,49 +19,28 @@ function resetTitle(){
 title.empty()
 }
 
-characters = [
-  {
-    name: "haunter",
-    type: 'ghost',
-    weakness: ['ghost'],
-    img: {
-      default: ""
+let haunter = {
+  name: "haunter",
+  img: {
+    default: ""
+  },
+  hp: 100,
+  attacks: [
+    {
+      name: "shadow ball",
+      dmg: 20,
     },
-    hp: {
-      current: 100,
+    {
+      name: "shadow claw",
+      dmg: 15,
     },
-    attacks: [
-      {
-        name: "shadow ball",
-        hp: randomNum(40,20),
-        avail: {
-          total: 30,
-          remaining: 30
-        }
-      },
-      {
-        name: "shadow claw",
-        hp: randomNum(60,45),
-        avail: {
-          total: 10,
-          remaining: 10
-        }
-      },
-      {
-        name: "curse",
-        hp: randomNum(75,60),
-        avail: {
-          total: 5,
-          remaining: 5
-        }
-      },
-      {
-        name: "astonish",
-        hp: randomNum(160, 130),
-        avail: {
-          total: 2,
-          remaining: 2
-        }
-      }
-    ]
-  } ]
+    {
+      name: "curse",
+      dmg: 5,
+    },
+    {
+      name: "astonish",
+      dmg: 15,
+    }
+  ]
+};
