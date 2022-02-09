0a9377b8a62bab5aaf975c2b41c356de83626a55
Mislam23 <islam.mohammad.student@gmail.com>
2022-02-09 11:39:47 -0500

    added text align funtion

---
+++ index.js
@@ -0,0 +1,12 @@
+//PAGE 1
+
+
+//TITLE
+let title = document.getElementById("title");
+
+
+//TEXT ALIGN
+let arrayToAlign = [title];
+arrayToAlign.forEach( el => {
+  el.style.textAlign = "center";
+})
