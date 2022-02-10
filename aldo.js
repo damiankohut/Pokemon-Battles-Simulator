d78c904b4120f49f0d8de51447376004cb067497
aldoortiz128 <aldoortiz128@gmail.com>
2022-02-10 13:46:42 -0500

    next page button in html

--- aldo.js
+++ aldo.js
@@ -1,6 +1,9 @@
-let button = document.createElement("button");
-document.getElementById("container").appendChild(button);
-button.addEventListener("click", () => {
-  document.getElementById("container").style.display = "none";
+// let button = document.createElement("button");
+// document.getElementById("container").appendChild(button);
+
+let startGame = document.getElementById("start")
+
+startGame.addEventListener("click", () => {
+  document.getElementById("middleContainer").style.display = "none";
   document.getElementById("title").style.display = "none";
 })
