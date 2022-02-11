e43b96256c329b8047536e60b0899a26995b08b9
Damian <Damiankohut@gmail.com>
2022-02-10 21:48:43 -0500

    hp added and attacks

--- index.js
+++ index.js
@@ -62,6 +62,31 @@ pokemon: null
 const player2 = {
 pokemon: null
 }
+//Damians code --------------------------------------
+let moveset1 = document.getElementById("moveset1")
+let moveset2 = document.getElementById("moveset2")
+let moveset3 = document.getElementById("moveset3")
+let moveset4 = document.getElementById("moveset4")
+
+
+function gameIsOver() {
+if (health2.value === 0 ) {
+window.alert('Player 1 won');
+} else if (health1.value === 0) {
+  window.alert('Player 2 won')
+} else {
+  if (turnOfPlayer == 1) {
+    document.getElementById('print')
+        .innerHTML = "Player 1 Turn";
+}
+else {
+    document.getElementById('print')
+        .innerHTML = "Player 2 Turn";
+}
+
+}
+}
+//Damians Code -----------------------------------------------
 
 //Box 1: Haunter
 function haunterSelect()  {
@@ -71,13 +96,57 @@ function haunterSelect()  {
   player1.player='Player1'
   player2Choice.innerText = `Player 2 Select`
   currPlayer = 'Player2'
+  //Damians code================================================
+  moveset1.innerText = "ShadowBall"
+  moveset2.innerText = "astonish"
+  moveset3.innerText = "shadowsneak"
+  moveset4.innerText = "shadowclaw"
+  moveset5.innerText = "sing"
+  moveset6.innerText = "strength"
+  moveset7.innerText = "smack"
+  moveset8.innerText = "bonk"
+  document.getElementById("moveset1").addEventListener("click", () =>{
+    shadowball()
+    gameIsOver()
+  })
+  document.getElementById("moveset2").addEventListener("click", () =>{
+    astonish()
+    gameIsOver()
+  })
+  document.getElementById("moveset3").addEventListener("click", () =>{
+    shadowsneak()
+    gameIsOver()
+  })
+  document.getElementById("moveset4").addEventListener("click", () =>{
+    shadowclaw()
+    gameIsOver()
+  })
+  document.getElementById("moveset5").addEventListener("click", () =>{
+    sing()
+    gameIsOver()
+  })
+  document.getElementById("moveset6").addEventListener("click", () =>{
+    strength()
+    gameIsOver()
+  })
+  document.getElementById("moveset7").addEventListener("click", () =>{
+    smack()
+    gameIsOver()
+  })
+  document.getElementById("moveset8").addEventListener("click", () =>{
+    bonk()
+    gameIsOver()
+  })
+  //end of damians code===============================================
   }
   if (currPlayer === 'Player2' && player1.pokemon!=haunter) {
   player2Choice.innerText = `You chose ${haunter.name}`
-  Player2.player='Player2'
-  player2.pokemon=haunter
+  Player2.player= 'Player2'
+  player2.pokemon = haunter
   }
 }
+
+
 //Box 2: JigglyPuff
 function jigglyPuffSelect() {
   if (currPlayer === 'Player1') {
@@ -86,11 +155,38 @@ function jigglyPuffSelect() {
   player1.player='Player1'
   player2Choice.innerText = `Player 2 Select`
   currPlayer='Player2'
+  //damians code start  ======================================
+  moveset1.innerText = "sing"
+  moveset2.innerText = "strength"
+  moveset3.innerText = "smack"
+  moveset4.innerText = "bonk"
+  moveset5.innerText = "ShadowBall"
+  moveset6.innerText = "astonish"
+  moveset7.innerText = "shadowsneak"
+  moveset8.innerText = "shadowclaw"
+  document.getElementById("moveset1").addEventListener("click", () =>{
+    shadowball()
+    gameIsOver()
+  })
+  document.getElementById("moveset2").addEventListener("click", () =>{
+    astonish()
+    gameIsOver()
+  })
+  document.getElementById("moveset3").addEventListener("click", () =>{
+    shadowsneak()
+    gameIsOver()
+  })
+  document.getElementById("moveset4").addEventListener("click", () =>{
+    shadowclaw()
+    gameIsOver()
+  })
+  //damians code end ============================================
 }
   if (currPlayer === 'Player2' && player1.pokemon!=jigglyPuff) {
   player2Choice.innerText = `You chose ${jigglyPuff.name}`
-  player2.pokemon=jigglyPuff
-  player2.player='Player2'
+  player2.pokemon = jigglyPuff
+  player2.player = 'Player2'
+
   }
 }
 
@@ -105,6 +201,3 @@ let player2Choice = document.getElementById("player2Choice")
 document.getElementById("char2").addEventListener("click", jigglyPuffSelect)
 
 
-
-
-  
--- mohammad.js
+++ mohammad.js
@@ -1,6 +1,6 @@
 //PAGE 2 GO TO PAGE 3
 let button2 = document.createElement("button");
-document.getElementById("container2").appendChild(button2);
+// document.getElementById("container2").appendChild(button2);
 button2.addEventListener("click", () => {
   document.getElementById("container2").style.display = "none";
   // document.getElementById("testing last page").style.display = "block";
