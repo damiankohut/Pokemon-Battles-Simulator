ed5b0ccabfacff915309ba8b7d0e2eb2ba91a407
aldoortiz128 <aldoortiz128@gmail.com>
2022-02-12 19:18:55 -0500

    DOM Refresh finished. Pages will now dynamically refresh at the end of battles, and when the player restarts the game. Minor aesthetic bug when a player replays the game--the previous pokemon images will still appear on the battlefield.

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 //PAGE 1
 let currPlayer = 'Player1'
+document.getElementById("startButton").disabled=true;
 
 // let button = document.createElement("button");
 // document.getElementById("container").appendChild(button);
@@ -66,73 +67,7 @@ document.getElementById("page2").appendChild(reset)
 reset.style.display = "none"
 //reset.disabled=true
 
- function endOfBattle() {
-  document.getElementById("moveset1").disabled = true
-  document.getElementById("moveset2").disabled = true
-  document.getElementById("moveset3").disabled = true
-  document.getElementById("moveset4").disabled = true
-  document.getElementById("moveset5").disabled = true
-  document.getElementById("moveset6").disabled = true
-  document.getElementById("moveset7").disabled = true
-  document.getElementById("moveset8").disabled = true
-  reset.style.display = "block"
-  
-}
-
-
-
-function gameIsOver(){
-if (health2.value === 0 ) {
-// let replay = document.createElement("button")
-// replay.className = "replay"
-// replay.innerHTML="Player 1 won. Rematch?"
-// document.getElementsByTagName("body").appendChild(replay)
-reset.style.display="block"
-reset.innerText="Hello"
-endOfBattle()
-reset.addEventListener("click", () => {
-document.getElementById("page2").style.display = "none"
-document.getElementById("title").style.display = "block"
-document.getElementById("page1").style.display = "block"
-document.getElementById("start").style.display = "block"
-document.getElementById("reset").style.display = "none"
-currPlayer = "Player1"
-}
-)
-
-
-
-//window.alert('Player 1 won'); 
-//location.reload();
-} else if (health1.value === 0) {
-  //window.alert('Player 2 won')
-reset.style.display="block"
-reset.innerText="Hello"
-endOfBattle()
-reset.addEventListener("click", () => {
-document.getElementById("page2").style.display = "none"
-document.getElementById("title").style.display = "block"
-document.getElementById("page1").style.display = "block"
-document.getElementById("start").style.display = "block"
-document.getElementById("reset").style.display = "none"
-currPlayer = "Player1"
-}
-)
-
-  //location.reload();
-} else {
-  if (turnOfPlayer == 1) {
-    document.getElementById('print')
-        .innerHTML = "Player 1 Turn";
-        
-}
-else {
-    document.getElementById('print')
-        .innerHTML = "Player 2 Turn";
-}
-}
-}
-
+ 
 //----------------------------------------
 //Aldo's Dynamic code
 //----------------------------------------
@@ -149,13 +84,16 @@ function Battlefield(){
 }
 //Box 1: Haunter
 function haunterSelect()  {
+  if (player1.pokemon === null || player2.pokemon === null) {
+    document.getElementById("startButton").disabled=true;
+  } else { document.getElementById("startButton").disabled=false;}
   if (currPlayer === 'Player1') {
   player1Choice.innerText = `You chose ${haunter.name}`
   player1.pokemon=haunter
   player1.player='Player1'
-  var img = document.createElement('img')
-  img.src = player1.pokemon.icon;
-  document.querySelector("#leftContainer").appendChild(img)
+  let p1Icon = document.createElement('img')
+  p1Icon.src = player1.pokemon.icon;
+  document.querySelector("#leftContainer").appendChild(p1Icon)
   //display player1 haunter in page2 - code start
   let haunter1ImgPage2 = document.createElement('img')
   haunter1ImgPage2.className = "haunter1ImgPage2";
@@ -222,9 +160,9 @@ function haunterSelect()  {
   player2Choice.innerText = `You chose ${haunter.name}`
   player2.player = 'Player2'
   player2.pokemon = haunter
-  var img = document.createElement('img')
-  img.src = player2.pokemon.icon;
-  document.querySelector("#rightContainer").appendChild(img)
+  var p2Icon = document.createElement('img')
+  p2Icon.src = player2.pokemon.icon;
+  document.querySelector("#rightContainer").appendChild(p2Icon)
   //display player2 haunter in page2 - code start
   let haunter2ImgPage2 = document.createElement('img')
   haunter2ImgPage2.className = "haunter2ImgPage2";
@@ -233,6 +171,9 @@ function haunterSelect()  {
   //display player2 haunter in page2 - code end
   currPlayer = null ////Sets to null until the next stage of game, prevents appending infinite Pokemon images
   }
+  if (player1.pokemon === null || player2.pokemon === null) {
+    document.getElementById("startButton").disabled=true;
+  } else {document.getElementById("startButton").disabled=false;}
 }
 //Box 2: JigglyPuff
 function jigglyPuffSelect() {
@@ -240,9 +181,9 @@ function jigglyPuffSelect() {
   player1Choice.innerText = `You chose ${jigglyPuff.name}`
   player1.pokemon=jigglyPuff
   player1.player='Player1'
-  var img = document.createElement('img')
-  img.src = player1.pokemon.icon;
-  document.querySelector("#leftContainer").appendChild(img)
+  var p1Icon = document.createElement('img')
+  p1Icon.src = player1.pokemon.icon;
+  document.querySelector("#leftContainer").appendChild(p1Icon)
   //display player1 jigglypuff in page2 - code start
   let jigglypuff1ImgPage2 = document.createElement('img')
   jigglypuff1ImgPage2.className = "jigglypuff1ImgPage2";
@@ -306,9 +247,9 @@ function jigglyPuffSelect() {
   player2Choice.innerText = `You chose ${jigglyPuff.name}`
   player2.pokemon = jigglyPuff
   player2.player = 'Player2'
-  var img = document.createElement('img')
-  img.src = player2.pokemon.icon;
-  document.querySelector("#rightContainer").appendChild(img)
+  var p2Icon = document.createElement('img')
+  p2Icon.src = player2.pokemon.icon;
+  document.querySelector("#rightContainer").appendChild(p2Icon)
   //display player2 jigglypuff in page2 - code start
   let jigglypuff2ImgPage2 = document.createElement('img')
   jigglypuff2ImgPage2.className = "jigglypuff2ImgPage2";
@@ -317,6 +258,9 @@ function jigglyPuffSelect() {
   //display player2 jigglypuff in page2 - code end
   currPlayer=null //Sets to null until the next stage of game, prevents appending infinite Pokemon images
   }
+  if (player1.pokemon === null || player2.pokemon === null) {
+    document.getElementById("startButton").disabled=true;
+  } else {document.getElementById("startButton").disabled=false;}
 }
 const jigglyPuff = new Pokemon('Jigglypuff', 200, "images/jigglypuff-front-gif.png", "images/jigglypuff-back-gif.png")
 const haunter = new Pokemon('Haunter', 200, "images/haunter-front-gif.png", "images/haunter-back-gif.png")
@@ -353,23 +297,162 @@ function disableMoves5To8() {
 
 
 let startGame = document.querySelector("#startButton")
-
 document.getElementById("movesets").style.display = "none";
 document.getElementById("winner-message").style.display = "none";
 
-
 startGame.addEventListener("click", () => {
+  document.getElementById("startButton").disabled=true;
+  reset.style.display = "none"
+  let rightContainer = document.querySelector("#rightContainer")
+  rightContainer.removeChild(rightContainer.lastElementChild)
+  let leftContainer = document.querySelector("#leftContainer")
+  leftContainer.removeChild(leftContainer.lastElementChild)
   document.getElementById("page1").style.display = "none";
   //document.getElementById("startButton").style.display = "none";
   document.getElementById("start").style.display = "none";
   document.getElementById("title").style.display = "none";
+  document.getElementById("page2").style.display = 'block';
   document.getElementById("movesets").style.display = 'block';
+
+  restartGame() //Damian JS
+  document.getElementById("moveset1").disabled = false
+  document.getElementById("moveset2").disabled = false
+  document.getElementById("moveset3").disabled = false
+  document.getElementById("moveset4").disabled = false
+  document.getElementById("moveset5").disabled = false
+  document.getElementById("moveset6").disabled = false
+  document.getElementById("moveset7").disabled = false
+  document.getElementById("moveset8").disabled = false
 // document.getElementById("winner-message").style.display = 'block';  this is for PAGE 3
 })
 
 //50% there
 // //Make button disabled unless pokemon are chosen
-// if (player1.pokemon === null || player2.pokemon === null) {
-//   document.getElementById("startButton").disabled=true;
-//   } 
-//   else  {document.getElementById("startButton").disabled=false;}
+
+
+function endOfBattle() {
+  document.getElementById("moveset1").disabled = true
+  document.getElementById("moveset2").disabled = true
+  document.getElementById("moveset3").disabled = true
+  document.getElementById("moveset4").disabled = true
+  document.getElementById("moveset5").disabled = true
+  document.getElementById("moveset6").disabled = true
+  document.getElementById("moveset7").disabled = true
+  document.getElementById("moveset8").disabled = true
+  reset.style.display = "block"
+  player1.pokemon=null
+  player2.pokemon=null
+  document.getElementById("char1").addEventListener("click", haunterSelect)
+  document.getElementById("char2").addEventListener("click", jigglyPuffSelect)
+}
+
+
+
+function gameIsOver(){
+
+if (health2.value === 0 ) {
+// let replay = document.createElement("button")
+// replay.className = "replay"
+// replay.innerHTML="Player 1 won. Rematch?"
+// document.getElementsByTagName("body").appendChild(replay)
+reset.style.display="block"
+reset.innerText="Hello"
+endOfBattle()
+reset.addEventListener("click", () => {
+// if (document.getElementsByClassName("jigglypuff1ImgPage2")){
+// let elements = document.getElementsByClassName("jigglypuff1ImgPage2");
+// elements[0].remove();
+// }
+// if (document.getElementsByClassName("jigglypuff2ImgPage2")){
+// let elements = document.getElementsByClassName("jigglypuff2ImgPage2");
+// elements[0].remove();
+// }
+// if (document.getElementsByClassName("haunter1ImgPage2")){
+// let elements = document.getElementsByClassName("haunter1ImgPage2");
+// elements[0].remove();
+// }
+// if (document.getElementsByClassName("haunter2ImgPage2")){
+// let elements = document.getElementsByClassName("haunter2ImgPage2");
+// elements[0].remove();
+// }
+document.getElementById("page2").style.display = "none"
+document.getElementById("title").style.display = "block"
+ document.getElementById("page1").style.display = "flex"
+ document.getElementById("middleContainer").style.display = "flex"
+ document.getElementById("middleContainer").style.width = "50vh"
+ document.getElementById("middleContainer").style.height = "50vh"
+ document.getElementById("middleContainer").style.justifyContent = "center"
+ document.getElementById("player1Choice").innerText = `Player 1 Select`
+ document.getElementById("player2Choice").innerText = `Player 2 Select`
+ document.getElementById("title").style.textAlign = "center"
+
+
+
+// document.getElementById("leftContainer").style.display = "block"
+// document.getElementById("middleContainer").style.display = "block"
+// document.getElementById("rightContainer").style.display = "block"
+document.getElementById("start").style.display = "block"
+currPlayer = "Player1"
+document.getElementById("reset").style.display = "none"
+}
+)
+
+
+
+//window.alert('Player 1 won'); 
+//location.reload();
+} else if (health1.value === 0) {
+  //window.alert('Player 2 won')
+reset.style.display="block"
+reset.innerText="Hello"
+endOfBattle()
+reset.addEventListener("click", () => {
+  // if (document.getElementsByClassName("jigglypuff1ImgPage2")){
+  //   let elements = document.getElementsByClassName("jigglypuff1ImgPage2");
+  //   elements[0].remove();
+  //   if (document.getElementsByClassName("jigglypuff2ImgPage2")){
+  //   let elements = document.getElementsByClassName("jigglypuff2ImgPage2");
+  //   elements[0].remove();
+  //   }
+  //   if (document.getElementsByClassName("haunter1ImgPage2")){
+  //   let elements = document.getElementsByClassName("haunter1ImgPage2");
+  //   elements[0].remove();
+  //   }
+  //   if (document.getElementsByClassName("haunter2ImgPage2")){
+  //   let elements = document.getElementsByClassName("haunter2ImgPage2");
+  //   elements[0].remove();
+  //   }
+document.getElementById("page2").style.display = "none"
+document.getElementById("title").style.display = "block"
+document.getElementById("page1").style.display = "flex"
+document.getElementById("middleContainer").style.display = "flex"
+document.getElementById("middleContainer").style.width = "50vh"
+document.getElementById("middleContainer").style.height = "50vh"
+document.getElementById("middleContainer").style.justifyContent = "center"
+document.getElementById("player1Choice").innerText = `Player 1 Select`
+ document.getElementById("player2Choice").innerText = `Player 2 Select`
+document.getElementById("title").style.textAlign = "center"
+
+
+
+
+currPlayer = "Player1"
+ 
+document.getElementById("start").style.display = "block"
+document.getElementById("reset").style.display = "none"
+}
+)
+
+  //location.reload();
+} else {
+  if (turnOfPlayer == 1) {
+    document.getElementById('print')
+        .innerHTML = "Player 1 Turn";
+        
+}
+else {
+    document.getElementById('print')
+        .innerHTML = "Player 2 Turn";
+}
+}
+}
